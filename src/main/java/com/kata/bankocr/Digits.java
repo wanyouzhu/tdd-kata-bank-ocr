220b2a480475b6b39a84ec8433db8c4666bbdedb
朱万友 <van@highsoft.ltd>
2019-08-15 00:12:48 +0800

    Refactored Digits.java

--- src/main/java/com/kata/bankocr/Digits.java
+++ src/main/java/com/kata/bankocr/Digits.java
@@ -5,31 +5,35 @@ import java.util.Collections;
 import java.util.List;
 
 public class Digits {
-    public static final List<String> ZERO = Collections.unmodifiableList(Arrays.asList(
+    public static final List<String> ZERO = defineDigit(
         " _ ",
         "| |",
         "|_|",
         "   "
-    ));
+    );
 
-    public static final List<String> ONE = Collections.unmodifiableList(Arrays.asList(
+    public static final List<String> ONE = defineDigit(
         "   ",
         "  |",
         "  |",
         "   "
-    ));
+    );
 
-    public static final List<String> TWO = Collections.unmodifiableList(Arrays.asList(
+    public static final List<String> TWO = defineDigit(
         " _ ",
         " _|",
         "|_ ",
         "   "
-    ));
+    );
 
-    public static final List<String> THREE = Collections.unmodifiableList(Arrays.asList(
+    public static final List<String> THREE = defineDigit(
         " _ ",
         " _|",
         " _|",
         "   "
-    ));
+    );
+
+    private static List<String> defineDigit(String... lines) {
+        return Collections.unmodifiableList(Arrays.asList(lines));
+    }
 }
