b91f30e3843ae7185aa521d5678ec65c64aa71bd
朱万友 <van@highsoft.ltd>
2019-08-14 16:00:31 +0800

    Added high-level tests for user story 1.

---
+++ src/main/java/com/kata/bankocr/BankOcr.java
@@ -0,0 +1,9 @@
+package com.kata.bankocr;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class BankOcr {
+    public void recognize(BufferedReader input, BufferedWriter output) {
+    }
+}
---
+++ src/test/java/com/kata/bankocr/BankOcrTest.java
@@ -0,0 +1,40 @@
+package com.kata.bankocr;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import static org.apache.commons.lang3.StringUtils.join;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BankOcrTest {
+    @Disabled
+    @Test
+    void should_recognize_input_file_and_produce_number_list_file() {
+        // Given
+        StringWriter output = new StringWriter();
+        BufferedReader input = new BufferedReader(new StringReader(join(new String[]{
+            "    _  _     _  _  _  _  _ ",
+            "  | _| _||_||_ |_   ||_||_|",
+            "  ||_  _|  | _||_|  ||_| _|",
+            "                           ",
+            " _  _  _  _  _  _        _ ",
+            "|_ |_| _||_   ||_ |_|  ||_|",
+            "|_| _| _| _|  ||_|  |  ||_|",
+            "                           ",
+        }, "\n")));
+
+        // When
+        new BankOcr().recognize(input, new BufferedWriter(output));
+
+        // Then
+        assertThat(output.toString()).isEqualTo(join(new String[]{
+            "123456890",
+            "693576418"
+        }, "\n"));
+    }
+}
