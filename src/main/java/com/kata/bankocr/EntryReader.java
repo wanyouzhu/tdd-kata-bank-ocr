c540241a92afe6aa3992ffb00fdc4904c52848f9
朱万友 <van@highsoft.ltd>
2019-08-14 23:08:34 +0800

    Refactored EntryReader.java

--- src/main/java/com/kata/bankocr/EntryReader.java
+++ src/main/java/com/kata/bankocr/EntryReader.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class EntryReader {
+    private static final int LINES_PER_ENTRY = 4;
     private final BufferedReader input;
 
     public EntryReader(BufferedReader input) {
@@ -14,7 +15,11 @@ public class EntryReader {
 
     public List<Entry> readAll() {
         List<String> lines = input.lines().collect(Collectors.toList());
-        int numberOfEntries = lines.size() / 4;
-        return IntStream.range(0, numberOfEntries).mapToObj(i -> new Entry(lines.subList(i * 4, i * 4 + 4))).collect(Collectors.toList());
+        int numberOfEntries = lines.size() / LINES_PER_ENTRY;
+        return IntStream.range(0, numberOfEntries).mapToObj(i -> extractEntry(lines, i)).collect(Collectors.toList());
+    }
+
+    private Entry extractEntry(List<String> lines, int index) {
+        return new Entry(lines.subList(index * LINES_PER_ENTRY, index * LINES_PER_ENTRY + LINES_PER_ENTRY));
     }
 }
