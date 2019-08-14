d38473d6f34cbda2c7376d708431d74ffb5edb5f
朱万友 <van@highsoft.ltd>
2019-08-15 00:01:52 +0800

    Refactored UnitTest.java

---
+++ src/main/java/com/kata/bankocr/Digits.java
@@ -0,0 +1,21 @@
+package com.kata.bankocr;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Digits {
+    public static final List<String> ZERO = Collections.unmodifiableList(Arrays.asList(
+        " _ ",
+        "| |",
+        "|_|",
+        "   "
+    ));
+
+    public static final List<String> TWO = Collections.unmodifiableList(Arrays.asList(
+        " _ ",
+        " _|",
+        "|_ ",
+        "   "
+    ));
+}
--- src/test/java/com/kata/bankocr/UnitTest.java
+++ src/test/java/com/kata/bankocr/UnitTest.java
@@ -2,37 +2,16 @@ package com.kata.bankocr;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UnitTest {
     @Test
     void should_equality_compare_to_others_correctly() {
-        assertThat(new Unit(two())).isEqualTo(new Unit(two()));
+        assertThat(new Unit(Digits.TWO)).isEqualTo(new Unit(Digits.TWO));
     }
 
     @Test
     void should_resolve_zero_correctly() {
-        assertThat(new Unit(zero()).resolve()).isEqualTo("0");
-    }
-
-    private List<String> zero() {
-        return Arrays.asList(
-            " _ ",
-            "| |",
-            "|_|",
-            "   "
-        );
-    }
-
-    private List<String> two() {
-        return Arrays.asList(
-            " _ ",
-            " _|",
-            "|_ ",
-            "   "
-        );
+        assertThat(new Unit(Digits.ZERO).resolve()).isEqualTo("0");
     }
 }
