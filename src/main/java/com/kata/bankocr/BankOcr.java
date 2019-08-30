e740020a03850f8dec995566ce574227979704b5
朱万友 <van@highsoft.ltd>
2019-08-30 18:04:03 +0800

    Refactored BankOcr.java

--- src/main/java/com/kata/bankocr/BankOcr.java
+++ src/main/java/com/kata/bankocr/BankOcr.java
@@ -3,25 +3,26 @@ package com.kata.bankocr;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.List;
 
 import static java.util.stream.Collectors.joining;
 
 public class BankOcr {
     public void recognize(BufferedReader input, BufferedWriter output) {
-        write(output, new EntryReader(input).readAll().stream().map(Entry::result).collect(joining("\n")));
-        flushOutput(output);
+        write(output, collectResult(getEntries(input)));
     }
 
-    private void write(BufferedWriter output, String line) {
-        try {
-            output.write(line);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private String collectResult(List<Entry> entries) {
+        return entries.stream().map(Entry::result).collect(joining("\n"));
     }
 
-    private void flushOutput(BufferedWriter output) {
+    private List<Entry> getEntries(BufferedReader input) {
+        return new EntryReader(input).readAll();
+    }
+
+    private void write(BufferedWriter output, String content) {
         try {
+            output.write(content);
             output.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
